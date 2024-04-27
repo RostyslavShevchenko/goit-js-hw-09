34329e17bf216bfa3f69790a49b2cb6bc841f797
RostyslavShevchenko <shevchenkorostyslav005@gmail.com>
2024-04-27 20:26:59 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
 
       rollupOptions: {
-        input: './src/*.html',
+        input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
